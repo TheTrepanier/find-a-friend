db861291e435d426100f30fb7027e3a1aff11970
TheTrepanier <thetrepanier1@Michaels-MacBook-Pro-3.local>
2019-05-14 19:10:41 -0700

    fixed quiz logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,8 +16,8 @@ module.exports = function (app) {
             };
 
             let trainerAnswers = req.body;
-            console.log(trainerAnswers);
-            console.log("^^^^");
+            // console.log(trainerAnswers);
+            // console.log("^^^^");
             
             
             let trainerScores = trainerAnswers.scores;
@@ -26,20 +26,22 @@ module.exports = function (app) {
             for (let index = 0; index < friends.length; index++) {
                 const element = friends[index];
                 totalDifference = 0;
+                console.log("This is for ", element.name);
+                console.log("=====");
+                
 
                 for (let i = 0; i < friends[index].socres.length; i++) {
                     const element = friends[index].socres[i];
+                    console.log(">>", Math.abs(Number(trainerScores[i] - Number(element))));
                     
                     totalDifference += Math.abs(Number(trainerScores[i]) - Number(element));
-                    
-                    if (totalDifference <= match.matchDiff) {
-                        console.log(totalDifference);
-                        console.log(friends[index].name);
-                        match.name = friends[index].name;
-                        match.photo = friends[index].photo;
-                        match.matchDiff = totalDifference;
-                    }
-
+                }
+                if (totalDifference <= match.matchDiff) {
+                    // console.log(totalDifference);
+                    console.log("New match is ", friends[index].name);
+                    match.name = friends[index].name;
+                    match.photo = friends[index].photo;
+                    match.matchDiff = totalDifference;
                 }
             }
             res.json(match);
